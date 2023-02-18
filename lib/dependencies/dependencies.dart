5169356d057f1b553d96c0bfe853220ae95fab44
Aditya Andar Rahim <adityandar@gmail.com>
2023-02-18 11:51:49 +0700

    First commit, Bismillah

--- lib/dependencies/dependencies.dart
+++ lib/dependencies/dependencies.dart
@@ -3,5 +3,4 @@ export 'package:cached_network_image/cached_network_image.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:fluttertoast/fluttertoast.dart';
-export 'package:go_router/go_router.dart';
 export 'package:timeago/timeago.dart';
--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -1,3 +1,2 @@
-export 'app_router.dart';
 export 'auto_route/app_router.dart';
 export 'routers.dart';
