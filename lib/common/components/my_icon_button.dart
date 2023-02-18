f4eaa0daa341498b23abaa1549666d5e607130e2
Aditya Andar Rahim <adityandar@gmail.com>
2023-02-18 16:53:47 +0700

    component: add new component(MyIconButton)

---
+++ lib/common/components/my_icon_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:showit/common/common.dart';
+import 'package:showit/dependencies/dependencies.dart';
+
+class MyIconButton extends StatelessWidget {
+  const MyIconButton({
+    super.key,
+    required this.icon,
+    required this.onTap,
+  });
+
+  final Icon icon;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(8.w),
+        decoration: BoxDecoration(
+          color: BrColor.neutralWhite,
+          boxShadow: BrShadow.def,
+          shape: BoxShape.circle,
+        ),
+        child: icon,
+      ),
+    );
+  }
+}
