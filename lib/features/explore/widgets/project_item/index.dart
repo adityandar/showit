31d063ef75da7d930be3fa994ad8829302cf76cb
Aditya Andar Rahim <adityandar@gmail.com>
2023-02-18 16:53:26 +0700

    component: move ProjectIconWidget to component

--- lib/features/explore/widgets/project_item/index.dart
+++ lib/features/explore/widgets/project_item/index.dart
@@ -1,4 +1,3 @@
-export 'project_icon_widget.dart';
 export 'project_item_body_widget.dart';
 export 'project_item_footer_widget.dart';
 export 'project_item_header_widget.dart';
--- lib/features/explore/widgets/project_item/project_item_widget.dart
+++ lib/features/explore/widgets/project_item/project_item_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:showit/common/common.dart';
+import 'package:showit/dependencies/dependencies.dart';
+import 'package:showit/settings/auto_route/app_router.dart';
 
 import 'index.dart';
 
@@ -9,21 +10,26 @@ class ProjectItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 16.w),
-      padding: EdgeInsets.all(16.w),
-      decoration: BoxDecoration(
-        boxShadow: BrShadow.def,
-        color: BrColor.neutralWhite,
-        borderRadius: BorderRadius.circular(12.r),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          ProjectItemHeaderWidget(),
-          ProjectItemBodyWidget(),
-          ProjectItemFooterWidget(),
-        ],
+    return GestureDetector(
+      onTap: () {
+        context.pushRoute(const ProjectDetailRoute());
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 16.w),
+        padding: EdgeInsets.all(16.w),
+        decoration: BoxDecoration(
+          boxShadow: BrShadow.def,
+          color: BrColor.neutralWhite,
+          borderRadius: BorderRadius.circular(12.r),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            ProjectItemHeaderWidget(),
+            ProjectItemBodyWidget(),
+            ProjectItemFooterWidget(),
+          ],
+        ),
       ),
     );
   }
