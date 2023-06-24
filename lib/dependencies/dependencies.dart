2250e9fa6a0a710aff4e9b38cb65c0c285aec89f
Aditya Andar Rahim <adityandar@gmail.com>
2023-06-25 06:39:52 +0700

    Add new dependencies

--- lib/dependencies/dependencies.dart
+++ lib/dependencies/dependencies.dart
@@ -1,9 +1,9 @@
 export 'package:auto_route/auto_route.dart';
 export 'package:cached_network_image/cached_network_image.dart';
+export 'package:flutter_rating_bar/flutter_rating_bar.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:flutter_sticky_header/flutter_sticky_header.dart';
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:fluttertoast/fluttertoast.dart';
-export 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 export 'package:sliver_tools/sliver_tools.dart';
 export 'package:timeago/timeago.dart';
