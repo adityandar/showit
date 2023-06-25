a5fe53b702494fd4664dabfabf2a0d41b052097e
Aditya Andar Rahim <adityandar@gmail.com>
2023-06-25 15:11:32 +0700

    Add Dependencies and assets

--- lib/dependencies/dependencies.dart
+++ lib/dependencies/dependencies.dart
@@ -1,5 +1,6 @@
 export 'package:auto_route/auto_route.dart';
 export 'package:cached_network_image/cached_network_image.dart';
+export 'package:equatable/equatable.dart';
 export 'package:flutter_rating_bar/flutter_rating_bar.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:flutter_sticky_header/flutter_sticky_header.dart';
